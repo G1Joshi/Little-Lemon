b24aca29689a5280d271c5d21809c7b863722a62
Jeevan Joshi <joshijeevan98@gmail.com>
2025-11-24 10:21:11 +0530

    setup app

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Jeevan Chandra Joshi on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
