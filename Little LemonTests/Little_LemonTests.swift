9f4cc0d1c0e6bcbe29cafda6ab1ab2093eb9e134
Jeevan Joshi <joshijeevan98@gmail.com>
2025-12-25 20:05:57 +0530

    add tests

---
+++ Little LemonTests/Little_LemonTests.swift
@@ -0,0 +1,20 @@
+//
+//  Little_LemonTests.swift
+//  Little LemonTests
+//
+//  Created by Jeevan Chandra Joshi on 25/12/25.
+//
+
+import XCTest
+
+final class Little_LemonTests: XCTestCase {
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testExample() throws {}
+
+    func testPerformanceExample() throws {
+        measure {}
+    }
+}
